a7321e3e8dee18271c9237f7f7fb1a2c168e0389
Abderrahmane Abdelouafi <118723356+ababdelo@users.noreply.github.com>
2023-08-14 14:30:04 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
-const menu=document.querySelector(".menu");
-const toggle=document.querySelector(".toggle");
-toggle.addEventListener("click",()=>{
+const menu = document.querySelector(".menu");
+const toggle = document.querySelector(".toggle");
+const socialIcons = document.querySelectorAll(".social-icon");
+
+toggle.addEventListener("click", () => {
+  if (!menu.classList.contains("active")) {
+    // Disable icon clicks when the menu is being activated
+    socialIcons.forEach(icon => icon.classList.add("disable-click"));
+  }
+
   menu.classList.toggle("active");
-})
+
+  if (menu.classList.contains("active")) {
+    // Enable icon clicks when the menu is fully activated
+    setTimeout(() => {
+      socialIcons.forEach(icon => icon.classList.remove("disable-click"));
+    }, 1250); // Adjust this time according to your animation time
+  }
+});
